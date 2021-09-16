2b2781263c65a06d444fc7ca0298950905c9634c
Diego Felipe Duran Lezama <diegoduran2001@gmail.com>
2021-09-16 16:35:40 -0400

    Ajout des commentaire sur le code MainMenu

--- ChessDevLogiciel/Assets/Scripts/MainMenu.cs
+++ ChessDevLogiciel/Assets/Scripts/MainMenu.cs
@@ -5,13 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    /*
+     * La méthode PlayGame(), je charge la scène ou le jeux va se dérouler.
+     */
     public void PlayGame()
     {
         SceneManager.LoadScene("SceneTest");
     }
-    
-    
-    
+    /*
+     * La méthode QuitGame() fait que lorsque le joueur clique sur le bouton quitter,
+     * le jeu arrête et l'exécutable se ferme.
+     */
     public void QuitGame()
     {
 #if UNITY_EDITOR
