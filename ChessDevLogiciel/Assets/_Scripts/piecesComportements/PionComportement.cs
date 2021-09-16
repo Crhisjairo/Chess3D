7fdd2e2a642dd1a6aeb9da27de73e3c12d0be174
Crhisjairo <59382477+Crhisjairo@users.noreply.github.com>
2021-09-16 16:51:01 -0400

    Mouvement des pions
    - Création de mouvement des pions.
    - Création du player controller de base.

---
+++ ChessDevLogiciel/Assets/_Scripts/piecesComportements/PionComportement.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PionComportement : MonoBehaviour, IPiece
+{
+   private Rigidbody _rb;
+
+   private bool _isSelected;
+   
+   private void Start()
+   {
+      _rb = GetComponent<Rigidbody>();
+   }
+
+   public void DeplacerPiece(Vector3 destination)
+   {
+      //Il faut calculer les déplacement possibles ici.
+      //Dans ce cas, c'est un Pion
+      
+      _rb.MovePosition(destination);
+   }
+   
+   public void SelectionnerPiece()
+   {
+      _isSelected = true;
+      
+      //On affiche les cases possibles du pion pour se déplacer
+   }
+
+   public void DeselectionnerPiece()
+   {
+      _isSelected = false;
+      
+      //On cache les cases disponibles
+   }
+
+   public bool IsSelected()
+   {
+      return _isSelected;
+   }
+
+   
+}
