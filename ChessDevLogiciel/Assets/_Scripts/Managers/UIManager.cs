3ff6cb7fe3930957cee1c3ec509b27865e346ab5
Dorensky <64221942+Dorensky@users.noreply.github.com>
2021-09-28 12:24:57 -0400

    Creation UI Manager & Comportement Chevalier

--- ChessDevLogiciel/Assets/_Scripts/Managers/UIManager.cs
+++ ChessDevLogiciel/Assets/_Scripts/Managers/UIManager.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public Slider timeSlider;
+    public Text timerText;
+    public float gameTime;
+    
+    private bool stopTimer;
+
     // Start is called before the first frame update
     void Start()
     {
+        stopTimer = false;
+        timeSlider.maxValue = gameTime;
+        timeSlider.value = gameTime;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float time = gameTime - Time.time;
+
+        int minutes = Mathf.FloorToInt(time / 60);
+        int seconds = Mathf.FloorToInt(time - minutes * 60f);
+
+        string textTime = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        if (time <= 0)
+        {
+            stopTimer = true;
+        }
+
+        if(stopTimer == false)
+        {
+            timerText.text = textTime;
+            timeSlider.value = time;
+        }
     }
 }
